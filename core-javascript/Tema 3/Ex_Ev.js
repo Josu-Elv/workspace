d377cd1bf2738a02fd631405657c6e4563e89480
Josu <elvirajosu@gmail.com>
2024-10-02 16:54:51 +0200

    Trabajando en ejercicio 3 corejs

--- core-javascript/Tema 3/Ex_Ev.js
+++ core-javascript/Tema 3/Ex_Ev.js
@@ -12,17 +12,17 @@ b) Si el diámetro es superior a 1.4 con un grosor inferior a 0.4, ó si el diá
 menor o igual a 1.4 pero mayor que 0.8, con un grosor inferior a 0.25, deberá
 mostrarse el mensaje “El grosor para esta rueda es inferior al recomendado” */
 
-//que me den un número positivo
 
 
-<script>
+
+
 function setDiameter() {
     let diameterValue;
     let diameter = prompt("¿De cuantos metros quieres el diametro de tu rueda?");
     if (diameter == null || diameter == "") {
     alert("Por favor, necesito que me des una medida");
     } else if (isNaN(diameter) || diameter <= 0) {
-    alert(" Necesito que me proporciones un número positivo")
+    alert(" Necesito que me proporciones un número positivo");
     } else { diameterValue = "Tu rueda tiene " + diameter + " metros de diametro";
     }
     document.getElementById("finalDiameter").innerHTML = diameterValue;
@@ -31,29 +31,22 @@ function setDiameter() {
     
 function setThickness() {  
     let thicknessValue;
-    let thickness = prompt("De cuantos metros quieres el grosor de tu rueda?")
+    let thickness = prompt("De cuantos metros quieres el grosor de tu rueda?");
     if (thickness == null || thickness == "") {
-    alert("Por favor, necesito que me des una medida")
+    alert("Por favor, necesito que me des una medida");
     } else if (typeof thickness !== "number" || thickness <= 0) {
-    alert(" Necesito que me proporciones un número positivo")
+    alert(" Necesito que me proporciones un número positivo");
     } else { thicknessValue = "Tu rueda tiene " + thickness + " mestros de grosor";
     }
-    document.getElementById(finalThickness) = thicknessValue
+    document.getElementById("finalThickness").innerHTML = thicknessValue;
 }
+/* 
+ a) Si el diámetro es superior a 1.4 debe mostrarse el mensaje “La rueda es para un
+vehículo grande”. Si es menor o igual a 1.4 pero mayor que 0.8 debe mostrarse el
+mensaje “La rueda es para un vehículo mediano”. Si no se cumplen ninguna de las
+condiciones anteriores debe mostrarse por pantalla el mensaje “La rueda es para
+un vehículo pequeño”.
+ */
 
 
 
-
-
-if (diameter === null) {
-    alert("Por favor necesito que me des una medida")
-} else if (typeof diameter !== "number" || diameter <= 0) {
-    alert(" Necesito que me proporciones un número positivo")
-} else { diameter === diameter}
-
-if (thickness === null) {
-    alert("Por favor necesito que me des una medida")
-} else if (typeof thickness !== "number" || thickness <= 0) {
-    alert(" Necesito que me proporciones un número positivo")
-} else { thickness === thickness}
-</script>
