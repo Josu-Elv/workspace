d7729ad16dd306ca000867e434e68dc3a126687d
Josu <elvirajosu@gmail.com>
2024-10-02 13:02:41 +0200

    Avanzando ejercicio 3 de core

--- core-javascript/Tema 3/Ex_Ev.js
+++ core-javascript/Tema 3/Ex_Ev.js
@@ -1,3 +1,7 @@
+/* Crea un script que pida al usuario el diámetro de una rueda y su grosor (en
+metros) y a través de condicionales if realice las siguientes operaciones:
+ */
+
 /* a) Si el diámetro es superior a 1.4 debe mostrarse el mensaje “La rueda es para un
 vehículo grande”. Si es menor o igual a 1.4 pero mayor que 0.8 debe mostrarse el
 mensaje “La rueda es para un vehículo mediano”. Si no se cumplen ninguna de las
@@ -11,9 +15,35 @@ mostrarse el mensaje “El grosor para esta rueda es inferior al recomendado” 
 //que me den un número positivo
 
 
+<script>
+function setDiameter() {
+    let diameterValue;
+    let diameter = prompt("¿De cuantos metros quieres el diametro de tu rueda?");
+    if (diameter == null || diameter == "") {
+    alert("Por favor, necesito que me des una medida");
+    } else if (isNaN(diameter) || diameter <= 0) {
+    alert(" Necesito que me proporciones un número positivo")
+    } else { diameterValue = "Tu rueda tiene " + diameter + " metros de diametro";
+    }
+    document.getElementById("finalDiameter").innerHTML = diameterValue;
+}
 
-let diameter = prompt("¿De cuantos metros quieres el diamtro de tu rueda?")
-let thickness = prompt("De cuantos metros quieres el grosor de tu rueda?")
+    
+function setThickness() {  
+    let thicknessValue;
+    let thickness = prompt("De cuantos metros quieres el grosor de tu rueda?")
+    if (thickness == null || thickness == "") {
+    alert("Por favor, necesito que me des una medida")
+    } else if (typeof thickness !== "number" || thickness <= 0) {
+    alert(" Necesito que me proporciones un número positivo")
+    } else { thicknessValue = "Tu rueda tiene " + thickness + " mestros de grosor";
+    }
+    document.getElementById(finalThickness) = thicknessValue
+}
+
+
+
+
 
 if (diameter === null) {
     alert("Por favor necesito que me des una medida")
@@ -21,3 +51,9 @@ if (diameter === null) {
     alert(" Necesito que me proporciones un número positivo")
 } else { diameter === diameter}
 
+if (thickness === null) {
+    alert("Por favor necesito que me des una medida")
+} else if (typeof thickness !== "number" || thickness <= 0) {
+    alert(" Necesito que me proporciones un número positivo")
+} else { thickness === thickness}
+</script>
