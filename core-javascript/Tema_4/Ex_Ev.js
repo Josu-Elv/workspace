806512a4b8d69eadaa7d79cd83ed9e4c0c7f8762
Josu <elvirajosu@gmail.com>
2024-10-04 16:08:01 +0200

    Ejercicio Tema 4 coreJs realiado

--- core-javascript/Tema_4/Ex_Ev.js
+++
@@ -1,7 +0,0 @@
-// Página web ejecutada en bucle infinito que pregunta por una
-// multiplicación y te responde con la solución mientras te vuelve a
-// preguntar de nuevo.
-
-
-const num1 =
-const num2 =
