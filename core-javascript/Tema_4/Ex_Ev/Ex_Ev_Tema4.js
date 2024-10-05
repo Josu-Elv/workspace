23bed70f9c7b49e5991f064af9f9406df97d0bbd
Josu <elvirajosu@gmail.com>
2024-10-05 11:14:59 +0200

    Amabas tareas t3 y t4 revisadas y con formato final

--- core-javascript/Tema_4/Ex_Ev/Ex_Ev_Tema4.js
+++ core-javascript/Tema_4/Ex_Ev/Ex_Ev_Tema4.js
@@ -1,19 +1,15 @@
 // Página web ejecutada en bucle infinito que pregunta por una
 // multiplicación y te responde con la solución mientras te vuelve a
 // preguntar de nuevo.
+
+
 function start_Infinite_Loop(){  
-do {
+while (true) { 
     const mult1 = prompt("Define el primer número");
     const mult2 = prompt("Define el segundo número");
         if (isNaN(mult1) || isNaN(mult2)) { 
         alert("Por favor introduce solo numeros");
         } else {alert(mult1*mult2)}
-    multiply = confirm("¿Quieres seguir multiplicando?");
-}    
-while (multiply);
+    }    
 } 
-
-
-
-   
 
